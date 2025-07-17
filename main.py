2468794a05c40749f766a58c33bb16f7e6f2ef6e
Chapelnine <not2binne@gmail.com>
2025-07-17 12:06:40 -0400

    Added player object that draws to screen, updated code to match Tutorial

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
 import pygame
 from constants import *
+from player import Player
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     print("Starting Asteroids!")
-    print(f"Screen width: {SCREEN_WIDTH}")
-    print(f"Screen height: {SCREEN_HEIGHT}")
+   # print(f"Screen width: {SCREEN_WIDTH}")
+   # print(f"Screen height: {SCREEN_HEIGHT}")
+    
+    clock = pygame.time.Clock()
+    dt = 0
+    player_object = Player((SCREEN_WIDTH/2), (SCREEN_HEIGHT/2))
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return       
-        pygame.Surface.fill(screen, (0, 0, 0))
+        screen.fill("black")
+        player_object.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
 
 if __name__ == "__main__":
     main()
