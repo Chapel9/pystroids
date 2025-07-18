26fc2b7c9e8405b80ab453c6eca94b862af72c83
Chapelnine <not2binne@gmail.com>
2025-07-18 14:11:09 -0400

    Added Rotation

--- main.py
+++ main.py
@@ -19,6 +19,7 @@ def main():
                 return       
         screen.fill("black")
         player_object.draw(screen)
+        player_object.update(dt)
         pygame.display.flip()
         dt = clock.tick(60)/1000
 
